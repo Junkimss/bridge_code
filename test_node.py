fbea928db5d399aef8eb63ee86e7298db43e53ee
Junkimss <144631631+Junkimss@users.noreply.github.com>
2023-09-24 18:16:26 +0900

    Add files via upload

--- test_node.py
+++ test_node.py
@@ -5,6 +5,9 @@ from std_msgs.msg import String
 from geometry_msgs.msg import Twist
 import random
 from std_srvs.srv import SetBool
+from rclpy.executors import MultiThreadedExecutor
+import threading
+import time
 
 # Node를 상속받아 사용
 class test_sub2(Node):
@@ -37,14 +40,34 @@ class test_sub2(Node):
     )
     self.service_client = self.create_client(
       SetBool,
-      "API/follow_me_mode"
+      "API/follow_me_mode",
       )
-    while not self.service_client.wait_for_service(timeout_sec=1):
+    while not self.service_client.wait_for_service(timeout_sec=10):
       self.get_logger().warning('The service_oprator service not available.')
 
-    # 1초마다 보내라.
-    self.timer = self.create_timer(1,self.call_back)
+    self.service_request =  SetBool.Request()
 
+
+    self.pub_thread = threading.Thread(target=self.pub_thread_function)
+    self.pub_thread.start()
+
+    self.client_thread = threading.Thread(target=self.client_thread_function)
+    self.client_thread.start()
+
+
+  def pub_thread_function(self):
+     while rclpy.ok():
+        try :
+          self.pub_msg()
+        finally:
+          time.sleep(1)
+
+  def client_thread_function(self):
+     while rclpy.ok():
+        try :
+          self.send_request()
+        finally:
+          time.sleep(1)
 
   # 수신 메시지 출력 함수.
   def sub_msg(self,msg):
@@ -56,37 +79,39 @@ class test_sub2(Node):
 
     twist_msg.angular.z = random.uniform(-0.5,0.5)
 
-
     # twist_msg 발송.
     self.pub.publish(twist_msg)
 
   def send_request(self):
-    service_request =  SetBool.Request()
-    service_request.data = self.toggle
-    service_response =self.service_client.call_async(service_request)
+    self.service_request.data = self.toggle
+    service_response = self.service_client.call_async(self.service_request)
+    service_response.add_done_callback(self.handle_service_response)  # 콜백 추가
 
-    if service_response.result() is not None:
-      response = service_response.result()
-      self.get_logger().info(f'Received response:{0} Received message {1}'.format(response.success,response.message))
-    else:
-      self.get_logger().error('Service call failed')
+  def handle_service_response(self, service_response):
+    try:
+        response = service_response.result()
+        self.get_logger().info(f'Received response: {response.success}, Received message: {response.message}')
+    except Exception as e:
+        self.get_logger().error(f'Service call failed: {e}')
 
     self.toggle = not self.toggle
 
-  def call_back(self):
-    self.pub_msg()
-    self.send_request()
 
 def main(args=None):
   rclpy.init(args=args)
   node = test_sub2()
-  try :
-    rclpy.spin(node)
-  except KeyboardInterrupt:
-      node.get_logger().info('Keyboard Interrupt (SIGINT)')
+  executor = MultiThreadedExecutor(num_threads = 2)
+  executor.add_node(node)
+  try:
+    try :
+        executor.spin()
+    except KeyboardInterrupt:
+        node.get_logger().info('Keyboard Interrupt (SIGINT)')
+    finally:
+        node.destroy_node()
+        executor.shutdown()
   finally:
-      node.destroy_node()
-      rclpy.shutdown()
+    rclpy.shutdown()
 
 if __name__ == '__main__':
   main()
